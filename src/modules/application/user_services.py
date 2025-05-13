90dbb6edbb6f0ba7bc552fb3f07294f1068140ee
palpasa4 <palpasa04@gmail.com>
2025-05-13 08:12:31 +0545

    User: added exceptions in service layer

--- src/modules/application/user_services.py
+++ src/modules/application/user_services.py
@@ -1,3 +1,4 @@
+import stat
 import hashlib, uuid
 from src.entrypoints.api.user.models import *
 from src.entrypoints.api.user.responses import *
@@ -18,12 +19,16 @@ class UserService():
 
 
     def check_duplicate_user(self, username:str) -> None:
-        user = self.user_repository.get_user_by_username(username)
-        if user:
-            logger.error(f"Duplicate user creation attempt: username= {user.username}")
-            raise DuplicateUserException(
-                message=f"User with username {user.username} already exists.", status_code=409
-            )
+        try:
+            user = self.user_repository.get_user_by_username(username)
+            if user:
+                logger.error(f"Duplicate user creation attempt: username= {user.username}")
+                raise DuplicateUserException(
+                    message=f"User with username {user.username} already exists.", status_code=409
+                )
+        except Exception as e:
+            logger.error(f"Database error while checking for duplicate user: {str(e)}")
+            raise UserDatabaseOperationError(message="Database error while checking for duplicate user", status_code=500)
 
 
     def check_user_details(self, entity: User) -> None:
@@ -49,17 +54,15 @@ class UserService():
 
 
     def create_user_handler(self, user_entity: User) -> User:
-        self.check_duplicate_user(user_entity.username)
-        self.check_user_details(user_entity)
         try:
+            self.check_duplicate_user(user_entity.username)
+            self.check_user_details(user_entity)
             self.user_repository.add_user(user_entity)
             self.create_bank_acc(user_entity)
             return user_entity
         except Exception as e:
-            logger.error(
-                f"Database error: Unable to add user '{user_entity.username}' to table 'user_data'.Error: {str(e)}"
-            )
-            raise DatabaseException("Database error: Unable to add user.", status_code=500)
+            logger.error(f"User creation failed for username='{user_entity.username}': {str(e)}")
+            raise UserDatabaseOperationError(message="Unable to create user.", status_code=500)
 
 
     def create_bank_acc(self, user_entity: User) -> None:
@@ -73,29 +76,37 @@ class UserService():
             logger.error(
                 f"Database error: Unable to create bank_acc for '{user_entity.username}'.Error: {str(e)}"
             )
-            raise DatabaseException(
+            raise UserDatabaseOperationError(
                 "Database error: Unable to create bank account.", status_code=500
             )
     
 
-    def check_valid_user(self, model: UserLoginModel) -> User: 
-        user = self.user_repository.get_user_by_username(model.username)
-        if user is None or not check_password(
-            model.password.get_secret_value(), str(user.password)
-        ):
-            logger.warning(f"Invalid user login attempt: username='{model.username}'")
-            raise InvalidUserLoginException(
-                message=f"Login Failed: Invalid username or password.", status_code=401
-            )
-        return user
-    
+    def check_valid_user(self, model: UserLoginModel) -> User:
+        try: 
+            user = self.user_repository.get_user_by_username(model.username)
+            if user is None or not check_password(
+                model.password.get_secret_value(), str(user.password)
+            ):
+                logger.warning(f"Invalid user login attempt: username='{model.username}'")
+                raise InvalidUserLoginException(
+                    message=f"Login Failed: Invalid username or password.", status_code=401
+                )
+            return user
+        except Exception as e:
+            logger.error(f"Database error while validating user. Error: {str(e)}")
+            raise UserDatabaseOperationError(message="Database error while validating user.",status_code=500)
+
 
     def check_ifuser(self, id: str) -> None:
-        if not self.user_repository.get_user_by_id(id):
-            logger.error(f"Unauthorized access attempt by user with userid: {id}")
-            raise UserPermissionDeniedException(
-                message="User not allowed.", status_code=401
-            ) 
+        try:
+            if not self.user_repository.get_user_by_id(id):
+                logger.error(f"Unauthorized access attempt by user with userid: {id}")
+                raise UserPermissionDeniedException(
+                    message="User not allowed.", status_code=401
+                ) 
+        except Exception as e:
+            logger.error(f"Database error while fetching user id. Error: {str(e)}")
+            raise UserDatabaseOperationError(message="Database error while validating user.",status_code=500)
         
 
     def deposit(self, transaction_entity: Transactions) -> BankAccount | None:
@@ -111,62 +122,70 @@ class UserService():
             return account_entity
         except Exception as e:
             logger.error(f"Database error: Unable to deposit amount for user with ID: {transaction_entity.cust_id}")
-            raise DatabaseException(
+            raise UserDatabaseOperationError(
                 message="Database error: Unable to deposit money.", status_code=500
             )
 
     
     def withdraw(self, transaction_entity: Transactions):
-        bank_acc= self.user_repository.get_account(transaction_entity.cust_id)
-        if transaction_entity.amount < 500:
-            logger.error(
-                "WithdrawBalanceException: Trying to withdraw less than minimum amount!"
-            )
-            raise ValidationException(
-                message="Minimum amount of withdrawal is 500!", status_code=400
-            )
-        if (
-            bank_acc
-            and isinstance(bank_acc.balance, (float))
-            and float(bank_acc.balance) - 500 < transaction_entity.amount
-        ):
-            logger.error(
-                "WithdrawBalanceException: Trying to withdraw more than existing balance!"
-            )
-            raise ValidationException(
-                message=f"Withdrawal exceeds existing balance. Minimum existing balance should be NPR 500 Existing balance is : {bank_acc.balance}",
-                status_code=400,
-            )
         try:
+            bank_acc= self.user_repository.get_account(transaction_entity.cust_id)
+            if transaction_entity.amount < 500:
+                logger.error(
+                    "WithdrawBalanceException: Trying to withdraw less than minimum amount!"
+                )
+                raise ValidationException(
+                    message="Minimum amount of withdrawal is 500!", status_code=400
+                )
+            if (
+                bank_acc
+                and isinstance(bank_acc.balance, (float))
+                and float(bank_acc.balance) - 500 < transaction_entity.amount
+            ):
+                logger.error(
+                    "WithdrawBalanceException: Trying to withdraw more than existing balance!"
+                )
+                raise ValidationException(
+                    message=f"Withdrawal exceeds existing balance. Minimum existing balance should be NPR 500 Existing balance is : {bank_acc.balance}",
+                    status_code=400,
+                )
             account_entity = self.user_repository.deduct_balance(transaction_entity)
             return account_entity
         except Exception as e:
             logger.error(
                 f"Database error: Unable to withdraw amount for user with ID: {transaction_entity.cust_id}"
             )
-            raise DatabaseException(
+            raise UserDatabaseOperationError(
                 message="Database error: Unable to withdraw money.", status_code=500
             )
        
 
     def user_view_details(self, id) -> UserViewDetails:
-        details = self.user_repository.get_detail(id)
-        if not details:
-            logger.error(
-                f"DatabaseException raised: Detail Not Found for user with ID: {id}."
-            )
-            raise DetailNotFoundException(message="Detail Not found.", status_code=404)
-        return details
-    
+        try:
+            details = self.user_repository.get_detail(id)
+            if not details:
+                logger.error(
+                    f"DatabaseException raised: Detail Not Found for user with ID: {id}."
+                )
+                raise DetailNotFoundException(message="Detail Not found.", status_code=404)
+            return details
+        except Exception as e:
+            logger.error(f"[User Access] Database error while retrieving user details. Error: {str(e)}")
+            raise UserDatabaseOperationError(message="Database error while retrieving user details.",status_code=500)
+
 
     def user_view_transactions(self, id: str)-> list[UserTransactionDetails]:
-        transactions = self.user_repository.get_transactions(id)
-        if not transactions:
-            logger.error(
-                f"DatabaseException: No transactions found for user with ID: {id}."
-            )
-            raise TransactionsNotFoundException(
-                message="No transactions found.", status_code=404
-            )
-        return transactions
+        try: 
+            transactions = self.user_repository.get_transactions(id)
+            if not transactions:
+                logger.error(
+                    f"DatabaseException: No transactions found for user with ID: {id}."
+                )
+                raise TransactionsNotFoundException(
+                    message="No transactions found.", status_code=404
+                )
+            return transactions
+        except Exception as e:
+            logger.error(f"[User Access] Database error while retrieving transaction details. Error: {str(e)}")
+            raise UserDatabaseOperationError(message="Database error while retrieving transaction details.",status_code=500)
     
